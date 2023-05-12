0364a9b620ee3606daeacb5196555e8ba6c8ba74
Chinedu Obinnah~ <cobinnah@yahoo.com>
2023-05-12 18:49:02 -0500

    Pratice variables

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string myName = "Chinedu";
+        int age = 26;
+        bool hasCar = true;
+        double avg = 56;
+        decimal cash = 3.00m;
+        char lastInitial= 'O';
+            
+        Console.WriteLine($"Hello my name is {myName} and I am {age} years old. It is {hasCar} that I have a car. I have {avg} baseball cards and ${cash} and my last initial is {lastInitial} ");
+
+    }
+}
